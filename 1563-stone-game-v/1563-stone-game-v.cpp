2aa0b0a39ef9744fe073773d3f44a75cb8f308e5
SANTHOSH RAJ T <santhoshrajtce@gmail.com>
2026-08-17 05:40:13 +0530

    Time: 235 ms (93.94%), Space: 27.7 MB (51.81%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -1,34 +1,28 @@
-class Solution {
+class Solution{
 public:
-    int stoneGameV(vector<int>& a) {
-        int n = a.size();
-        vector<int> pref(n + 1, 0);
-        for(int i = 0 ; i < n ; i++) {
-            pref[i] = a[i];
-            if(i > 0) pref[i] += pref[i - 1];
-        }
-        auto getSum = [&](int l, int r) {
-            if(l > r) return 0;
-            return l == 0 ? pref[r] : pref[r] - pref[l - 1];
-        };
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        for(int len = 2; len <= n; len++) {
-            for(int i = 0; i < n; i++) {
-                int j = i + len - 1;
-                if(j >= n) continue;
-                for(int k = i; k < j; k++) {
-                    int left = getSum(i, k);
-                    int right = getSum(k + 1, j);
-                    if(left < right) {
-                        dp[i][j] = max(dp[i][j] , left + dp[i][k]);
-                    } else if(right < left) {
-                        dp[i][j] = max(dp[i][j] , right + dp[k + 1][j]);
-                    } else {
-                        dp[i][j] = max({dp[i][j] , right + dp[i][k], left + dp[k + 1][j]});
-                    }
-                }
+  int stoneGameV(vector<int>&a){
+    int n=a.size(),ans=0;
+    vector<vector<int>>dp(n,vector<int>(n,-1));
+    dp[0][n-1]=0;
+    for(int i=0;i<n;i++){
+      int tot=0;
+      for(int k=i;k<n;k++)tot+=a[k];
+      for(int j=n-1;j>=i;j--){
+        if(dp[i][j]>=0){
+          if(i==j)ans=max(ans,dp[i][j]);
+          else{
+            int s1=0;
+            for(int l=i;l<j;l++){
+              s1+=a[l];
+              int s2=tot-s1;
+              if(s1>=s2)dp[l+1][j]=max(dp[l+1][j],s2+dp[i][j]);
+              if(s1<=s2)dp[i][l]=max(dp[i][l],s1+dp[i][j]);
             }
+          }
         }
-        return dp[0][n - 1];
+        tot-=a[j];
+      }
     }
+    return ans;
+  }
 };
