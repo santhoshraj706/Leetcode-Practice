7b6c9c0903132809f0e6b854317c613cfe3d1ac5
SANTHOSH RAJ T <santhoshrajtce@gmail.com>
2026-09-12 16:28:09 +0530

    Time: 16 ms (21.05%), Space: 46.7 MB (16.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> freq=new HashMap<>();
+        for(char c:s.toCharArray()){
+            freq.put(c,freq.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            if(!freq.containsKey(c)){
+                return false;
+            }
+            freq.put(c,freq.get(c)-1);
+            if(freq.get(c)==0){
+                freq.remove(c);
+            }
+        }
+        return freq.isEmpty();
+    }
+}
